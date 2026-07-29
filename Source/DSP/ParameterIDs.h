bcc804b0a5451b5efa65fa811b2ac7e14e68688d
jpeg1q <141069364+jpeg1q@users.noreply.github.com>
2026-07-29 10:12:16 -0500

    Add Source/DSP/ParameterIDs.h

---
+++ Source/DSP/ParameterIDs.h
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+#pragma once
+
+namespace bellows::param
+{
+inline constexpr auto low = "low";
+inline constexpr auto middle = "middle";
+inline constexpr auto musette = "musette";
+inline constexpr auto high = "high";
+inline constexpr auto bellows = "bellows";
+inline constexpr auto attack = "attack";
+inline constexpr auto release = "release";
+inline constexpr auto brightness = "brightness";
+inline constexpr auto musetteWidth = "musetteWidth";
+inline constexpr auto variation = "variation";
+inline constexpr auto mechanical = "mechanical";
+inline constexpr auto air = "air";
+inline constexpr auto body = "body";
+inline constexpr auto room = "room";
+inline constexpr auto output = "output";
+} // namespace bellows::param
